ff845527bdb975d1fe5f11829874e3c74cdb1310
Cristi Sandu <sandu.cristi@yahoo.com>
2023-06-29 21:24:15 +0300

    Add project files.

---
+++ lab1ex5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Program care va afisa in ordine crescatoare doua numere de la tastatura.*/
+
+            int x;
+            int y;
+
+            Console.WriteLine("Introduceti primul numar:");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al doilea numar:");
+            y = int.Parse(Console.ReadLine());
+
+
+            if (x < y)
+            {
+                Console.Write("\n" + x + ";");
+                Console.WriteLine(y);
+            }
+            else if (x > y)
+            {
+                Console.Write("\n" + y + ";");
+                Console.WriteLine(x);
+            }
+            else
+            {
+                Console.WriteLine("\n" + "Cele doua numere sunt egale" + ".");
+            }
+
+        }
+    }
+}
